f3d4155e6f67532d86b5e09b3e2cc9c3cd05a002
ykiseong303 <41833412+ykiseong303@users.noreply.github.com>
2021-08-06 00:02:04 +0900

    Update SWEA_5215_2.java

--- SWEA_PS/recursive/SWEA_5215_2.java
+++ SWEA_PS/recursive/SWEA_5215_2.java
@@ -12,7 +12,9 @@ import java.util.StringTokenizer;
  * 접근 : N가지 재료에 대한 조합(순서의미x, 중복x)
  * 
  * 구현 : 부분집합과 조합으로 생각할 수 있음. 
- * 		 
+ * 	 부분집합
+ * 	 1. 현재위치를 고르고 재귀 호출 
+ * 	 2. 종료조건을 만나면 직전호출로 돌아가서 고르지 않는 경우에 대해 재귀호출
  */
 public class SWEA_5215_2 {
 	static int[] score;
@@ -45,26 +47,26 @@ public class SWEA_5215_2 {
 	}
 	private static void powerSet(int idx,int cnt) {
 		
-		if(cnt == N) {
+		if(cnt == N) { // 종료조건 
 			int kcal_sum = 0;
 			int score_sum = 0;
 			for(int i=0;i<N;i++) {
-				if(visited[i]==true) {
-					kcal_sum += kcal[i];
+				if(visited[i]==true) { // 고른 요소들에 한해서만 
+					kcal_sum += kcal[i]; // 각각의 합계를 계산 
 					score_sum += score[i];
 				}
 			}
-			if(kcal_sum<=limit) {
+			if(kcal_sum<=limit) { // 제한 값을 초과하지 않는 범위에서 최댓값 갱신 
 				res = Math.max(res, score_sum);
 			} 
 			return;
 		}
 		
 		
-		visited[idx] = true;
-		powerSet(idx+1,cnt+1);
-		visited[idx] = false;
-		powerSet(idx+1,cnt+1);
+		visited[idx] = true; // 현재위치를 고르는 경우 
+		powerSet(idx+1,cnt+1); // 재귀호출 
+		visited[idx] = false; // 현재위치를 고르지 않는 경우 
+		powerSet(idx+1,cnt+1); // 재귀호출 
 		
 	}
 
