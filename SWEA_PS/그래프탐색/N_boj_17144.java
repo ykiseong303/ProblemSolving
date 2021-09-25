5dc5e9d6d812936827bea78a6218856ce708570e
ykiseong303 <41833412+ykiseong303@users.noreply.github.com>
2021-09-25 23:52:18 +0900

    Update N_boj_17144.java

--- SWEA_PS/그래프탐색/N_boj_17144.java
+++ SWEA_PS/그래프탐색/N_boj_17144.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Queue; 
 import java.util.StringTokenizer;
 
 public class N_boj_17144 {
