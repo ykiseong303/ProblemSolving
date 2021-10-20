8f86f83e08679261930a40021c108b30168da9c1
ykiseong303 <41833412+ykiseong303@users.noreply.github.com>
2021-10-20 23:48:24 +0900

    Update R_boj_3109.java

--- SWEA_PS/recursive/R_boj_3109.java
+++ SWEA_PS/recursive/R_boj_3109.java
@@ -13,7 +13,7 @@ public class R_boj_3109 {
 	static int target;
 	static char[][]map;
 	static int[]dx = {-1,0,1};
-	static int[]dy = {1,1,1};
+	static int[]dy = {1,1,1};  
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		R = sc.nextInt();
